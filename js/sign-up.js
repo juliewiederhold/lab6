dba7c6468d47876901bc239acea40ad727b65b60
unknown <iguest@is-m430m27-29.ischool.uw.edu>
2013-11-01 09:13:15 -0700

    Lab 6 - to submit

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,3 +1,39 @@
 //signup.js 
 // add your JavaScript code to this file
 
+$(function(){
+	var stateSelect = $('.us-states');
+	var option;
+	var idx;
+	var state;
+
+	for(idx = 0; idx <usStates.length; ++idx) {
+		state = usStates[idx];
+		option = $(document.createElement('option'));
+		option.attr('value', state.abbreviation);
+		option.html(state.name);
+		stateSelect.append(option);
+	}
+
+	$('.signup-form').submit(function(){
+		var signupForm = $(this);
+		var addr1Input = signupForm.find('input[name="addr-1"]');
+		var addr1Value = addr1Input.val();
+		var zipInput;
+		var zipValue;
+
+		if(addr1Value && addr1Value.length > 0) {
+			zipInput = signupForm.find('input[name="zip"]');
+			zipValue = zipInput.val();
+			if(zipValue == null || zipValue.length == 0){
+				alert('Please enter your zip code.');
+				return false;
+			} else
+				return true;
+		}
+	});
+
+	$('.cancel-signup').click(function(){
+   		window.location = 'http://www.google.com';
+	});
+});
